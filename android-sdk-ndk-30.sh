06b296b76f257b765ed88c617c29ef8135f3396b
e2dk4r <43293320+e2dk4r@users.noreply.github.com>
2022-11-06 06:51:41 +0300

    30: add messages after each process

--- android-sdk-ndk-30.sh
+++ android-sdk-ndk-30.sh
@@ -17,6 +17,7 @@ echo 'f3f5b75744dbf6ee6ed3e8174a71e513bfee502d0bc3463ea97e517bff68d84e  platform
 unzip -q platform-30_r03.zip
 mkdir -p $ANDROID_HOME/platforms
 mv android-11 $ANDROID_HOME/platforms/android-30
+echo 'installed platform android-30'
 
 test -f android-ndk-r25b-linux.zip || curl -LO https://dl.google.com/android/repository/android-ndk-r25b-linux.zip
 echo '403ac3e3020dd0db63a848dcaba6ceb2603bf64de90949d5c4361f848e44b005  android-ndk-r25b-linux.zip' | sha256sum -c
@@ -30,15 +31,18 @@ echo '18f9b8f27ea656e06b05d8f14d881df8e19803c9221c0be3e801632bcef18bed  3534162-
 unzip -q 3534162-studio.sdk-patcher.zip
 mkdir -p $ANDROID_HOME/patcher
 mv sdk-patcher $ANDROID_HOME/patcher/v4
+echo 'installed ndk 25.1.8937393'
 
 test -f build-tools_r30-linux.zip || curl -LO https://dl.google.com/android/repository/build-tools_r30-linux.zip
 echo 'ed3b7f9b2d15e90a12c2e739adb749d7d834e2f953e677380206bd14db135c6c  build-tools_r30-linux.zip' | sha256sum -c
 unzip -q build-tools_r30-linux.zip
 mkdir -p $ANDROID_HOME/build-tools
 mv android-11 $ANDROID_HOME/build-tools/30.0.0
+echo 'installed build-tools 30.0.0'
 
 # optional
 #test -f emulator-linux_x64-9189900.zip || curl -LO https://dl.google.com/android/repository/emulator-linux_x64-9189900.zip
 #echo '5d18602e9a4e39eb88d01a243fe0df061dcec945042e385dafa4de4a848d29ce  emulator-linux_x64-9189900.zip' | sha256sum -c
 #unzip -q emulator-linux_x64-9189900.zip
 #mv $ANDROID_HOME/emulator 
+#echo 'installed emulator'
